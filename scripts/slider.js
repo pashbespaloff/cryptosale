44a58588fb50aebffe157a7382f9d6f0f9b023eb
pash bespaloff <123310036+pashbespaloff@users.noreply.github.com>
2023-07-06 19:03:41 +0200

    slider added

---
+++ scripts/slider.js
@@ -0,0 +1,24 @@
+const sliderButtons = document.querySelectorAll(".slider-buttons__radio"),
+      applications = document.querySelectorAll(".latest-applications__app");
+
+const moveSlide = (e) => {
+  const slides = Array.from(applications);
+  slides.forEach(slide => {
+    slide.classList.add("animate-slide");
+    slide.classList.remove("active-slide");
+  });
+
+
+  for (let i = 0; i < slides.length; i++) {
+    const slide = slides[i];
+    if (slide.dataset.id === e.target.id) {
+      slide.classList.add("active-slide");
+    };
+  };
+
+  setTimeout(() => {
+    slides.forEach(slide => slide.classList.remove("animate-slide"));
+  }, 500);
+};     
+
+sliderButtons.forEach(item => item.addEventListener("click", moveSlide));
